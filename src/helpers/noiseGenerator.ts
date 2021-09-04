e43d49668b7f7357edcceca0ef32b8646b8a6ae9
Grzegorz Pabis <grzegorz@everywhere.pl>
2021-09-04 18:06:21 +0200

    Optimalization

--- src/helpers/noiseGenerator.ts
+++ src/helpers/noiseGenerator.ts
@@ -14,14 +14,13 @@ export class NoiseGenerator {
     }
 
     generateNoise = () => {
-        const map: number[][] = [[]];
+        const map: number[][] = new Array(this.height);
         const mainOutline = makeRectangle(this.width, this.height, this.noise, {frequency: 0.03,  amplitude: 1})
         const boulders = makeRectangle(this.width, this.height, this.noise, {frequency: 0.09,  amplitude: 0.5})
         const smallRocks = makeRectangle(this.width, this.height, this.noise, {frequency:0.27, amplitude: 0.25})
-        console.log(mainOutline);
-        for(let y = 0; y < mainOutline.length; y++){
-            map.push([]);
-            for(let x = 0; x < mainOutline[0].length; x++){
+        for(let y = 0; y < this.height; y++){
+            map[y] = new Array(this.width);
+            for(let x = 0; x < this.width; x++){
                 map[y].push(mainOutline[y][x] + boulders[y][x] + smallRocks[y][x]);
             }
         }
